e186df79cdd36de731d0609a4a7fd76e5270b254
o.ermakov <o.ermakov@cian.ru>
2019-08-16 15:52:38 +0300

    Fix python 2.7 support

--- src/attributes_doc.py
+++ src/attributes_doc.py
@@ -1,12 +1,19 @@
 import ast
+import sys
 import inspect
 import textwrap
-from typing import Dict, Type, TypeVar
+from typing import Dict, Type, TypeVar, Tuple
 
 
 __all__ = ['get_attributes_doc', 'attributes_doc']
 
+PY35 = sys.version_info[0:2] >= (3, 5)
+
 T = TypeVar('T')
+
+assign_stmts = (ast.Assign, )  # type: Tuple[Type[ast.stmt], ...]
+if PY35:
+    assign_stmts = (ast.Assign, ast.AnnAssign)
 
 
 class FStringFound(Exception):
@@ -26,15 +33,15 @@ def get_attributes_doc(cls):
         cls_ast = module.body[0]
         for stmt1, stmt2 in zip(cls_ast.body, cls_ast.body[1:]):  # type: ignore
             if (
-                    not isinstance(stmt1, (ast.AnnAssign, ast.Assign))
+                    not isinstance(stmt1, assign_stmts)
                     or not isinstance(stmt2, ast.Expr)
             ):
                 continue
             doc_expr_value = stmt2.value
-            if isinstance(doc_expr_value, ast.JoinedStr):
+            if PY35 and isinstance(doc_expr_value, ast.JoinedStr):
                 raise FStringFound
             if isinstance(doc_expr_value, ast.Str):
-                if isinstance(stmt1, ast.AnnAssign):
+                if PY35 and isinstance(stmt1, ast.AnnAssign):
                     attr_names = [stmt1.target.id]  # type: ignore
                 else:
                     attr_names = [target.id for target in stmt1.targets]  # type: ignore
@@ -46,5 +53,5 @@ def get_attributes_doc(cls):
 def attributes_doc(cls):
     # type: (Type[T]) -> Type[T]
     for attr_name, attr_doc in get_attributes_doc(cls).items():
-        setattr(cls, f'__doc_{attr_name}__', attr_doc)
+        setattr(cls, '__doc_%s__' % attr_name, attr_doc)
     return cls
